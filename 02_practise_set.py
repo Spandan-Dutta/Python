2b8111485b1d8c7995fd563731e9698e71440341
Spandan Dutta <67653554+spandy07@users.noreply.github.com>
2020-11-12 16:44:39 +0530

    Add files via upload

---
+++ 02_practise_set.py
@@ -0,0 +1,29 @@
+# The sum of two numbers
+a = 6
+b = 4 
+print("The sum of a+b is " , a+b)
+
+# Write a python Program to find remainder when a number is divided by 2
+a = 5 
+print("The remainder of a when divided by 2 is " , a%2)
+
+# Check the type of the variable assigned using input() function
+a = input("Emter your name")
+print(type(a))
+
+# Use comparison opearators to find out whether a given variable 'a' is greater than 'b' or not. Take a = 34 and b = 80.
+a = 34
+b = 80
+c = a > b
+print(c)
+
+# Write a python program to find average of two numbers entered by the user.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = (a+b)/2
+print("The average of a and b is " , c)
+
+# Write a python program to calculate square of a number entered by the user.
+a = int(input("Enter a number: "))
+b = a*a
+print("The square of a number is: " , b)
